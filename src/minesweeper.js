47e3b70d8e8324fe1196afd0f3059bb39c610310
Grace Brassard-Monahan <gracemonahan4@gmail.com>
2019-05-21 19:22:37 +0200

    check neighboring tiles for bombs without checking non-existant tiles

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -17,12 +17,35 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
     } board.push(row);
   }
 
+//this code checks neighboring tiles for bombs without checking non-existant tiles
+  const getNumberOfNeighborBombs = (bombBoard, rowIndex, columnIndex) => {
+    const neighborOffsets = [[1, 0], [-1, 0], [1, 1], [0, 1], [-1, 1], [0, -1], [1, -1], [-1, -1]];
+    const numberOfRows = bombBoard.length;
+    const numberOfColumns = bombBoard[0].length;
+    let numberOfBombs = 0;
+
+    neighborOffsets.forEach(offset => {
+      const neighborRowIndex = rowIndex + offset[0];
+      const neighborColumnIndex = columnIndex + offset[1];
+      if ((neighborRowIndex >= 0) && (neighborRowIndex <= numberOfRows) && (neighborColumnIndex >= 0) && (neighborColumnIndex <= numberOfColumns)) {
+        if (bombBoard[neighborRowIndex][neighborColumnIndex] === 'B') {
+          numberOfBombs++
+        }
+      }
+    });
+    return numberOfBombs;
+  };
+
   let numberOfBombsPlaced = 0;
   while (numberOfBombsPlaced < numberOfBombs)
   {
   //code to find a random integer
   let randomRowIndex = Math.floor(Math.random() * numberOfRows);
   let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+  if (board[randomRowIndex][randomColumnIndex] !== 'B') {
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced++;
+  }
 
   board[randomRowIndex][randomColumnIndex] = 'B';
   numberOfBombsPlaced++;
